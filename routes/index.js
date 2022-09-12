fb44baa638f2fc79db4756929ebf1d5dd1b4c627
Teddy <tedloukas@outlook.com>
2022-09-12 17:47:30 +0900

    push to Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date(),
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date(),
-    },
-  ];
-  res.render("index", { title: "yolo", message: messages });
+  res.render("index", { title: "Message Board", message: messages });
 });
 router.get("/hello", function (req, res, next) {
-  res.render("hello", { title: "MOTHAFUCKA" });
+  res.render("hello", { title: "Hello" });
+});
+router.get("/new", (req, res, next) => {
+  res.render("new", { title: "NEW" });
+});
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.text,
+    name: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 module.exports = router;
